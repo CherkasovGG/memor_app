0c0d395e5608d4a42ff4ad2f873a4d2a2c4d673e
CherkasovGG <159588500+CherkasovGG@users.noreply.github.com>
2025-03-25 20:49:33 +0300

    Update config.js

--- src/config.js
+++ src/config.js
@@ -7,7 +7,7 @@
 // }
 
 const config = {
-    baseURL: "http://185.233.187.23",
+    baseURL: "https://proxy.memorand.ru",
     authBaseURL: "http://localhost:8000",
     notesBaseURL: "http://localhost:8000",
     mediaBaseURL: "http://localhost:8000",
